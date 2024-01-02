5c5506d6e2bf9a4da593353b31c23c77257452a6
Benard Haxhijaj <benardhaxhijaj@gmail.com>
2024-01-02 21:01:57 +0100

    dark future

--- facts/src/App.jsx
+++ facts/src/App.jsx
@@ -1,11 +1,18 @@
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
+import React from "react";
 
 const App = () => {
+  const [dark, setDark] = React.useState(false);
+
+  const toggleDarkMode = () => {
+    setDark((prevDark) => !prevDark);
+  };
+
   return (
     <div className="container">
-      <Navbar />
-      <Main />
+      <Navbar darkMode={dark} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={dark} />
     </div>
   );
 };
